6c08a47b36bee143c3729420a5acef4c6099d7b3
Anka <runner@Mac-1734801425758.local>
2024-12-21 18:41:40 +0000

    mas 1.9.5

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -6,8 +6,8 @@ class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/mas-cli/mas"
   url "https://github.com/mas-cli/mas.git",
-      tag:      "v1.8.8-beta.13",
-      revision: "5a6314fe36056bd077ee0b1c08924b169083b9d1"
+      tag:      "v1.9.5",
+      revision: "c3da5f346f318fe18b97b9a7023341a5d977545b"
   license "MIT"
   head "https://github.com/mas-cli/mas.git", branch: "main"
 
